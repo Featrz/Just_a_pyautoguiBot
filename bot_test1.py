f5f2ee18799f634d9991c22bfe7058c17243a07f
Featrz <81426878+Featrz@users.noreply.github.com>
2021-04-28 23:02:40 +0300

    Add files via upload

---
+++ bot_test1.py
@@ -0,0 +1,33 @@
+import keyboard
+import pyautogui as pg
+from time import *
+sleep(2)
+def MagicPianoBot():
+    #3 columns: (762, 400), (839, 400), (949, 400)
+    while keyboard.is_pressed('q') == False:
+        if pg.pixel(762, 400)[0] == 0:
+            pg.mouseDown(762, 400)
+        if pg.pixel(798, 400)[0] == 0:
+            pg.mouseDown(798, 400)
+        if pg.pixel(881, 400)[0] == 0:
+            pg.mouseDown(881, 400)
+        if pg.pixel(949, 400)[0] == 0:
+            pg.mouseDown(949, 400)
+#rgb(255,242,234)
+def PositionCursor():
+    #(634, 328) --- (1270, 843)
+    while keyboard.is_pressed('q') == False:
+        pg.sleep(2)
+        print(pg.position())
+def AimBoost():
+    while keyboard.is_pressed('q') == False:
+        pic = pg.screenshot(region=(660, 350, 740, 520))
+        w, h = pic.size
+        for x in range(0, w, 5):
+            for y in range(0, h, 5):
+                r, g, b = pic.getpixel((x, y))
+                if b == 195:
+                    pg.mouseDown(x + 660, y + 350)
+                    pg.sleep(0.1)
+                    break
+
